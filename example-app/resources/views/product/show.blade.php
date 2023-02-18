c43badb410200f2bf23ecc1a07f0b240df377c00
samuVillegas <villegassamuel25@gmail.com>
2023-02-18 17:19:53 -0500

    Upload code

---
+++ example-app/resources/views/product/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('subtitle', $viewData["subtitle"])
+@section('content')
+<div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">
+                    {{ $viewData["product"]["name"] }}
+                </h5>
+                <p class="card-text">{{ $viewData["product"]["description"] }}</p>
+                @if ($viewData["product"]["description"] > 100)
+                <p class="card-text text-danger">{{ $viewData["product"]["price"] }}</p>
+                @else
+                <p class="card-text">{{ $viewData["product"]["price"] }}</p>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ example-app/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
+Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name("home.contact");
+
+//Product routes
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show")->whereNumber('id');
